4030a5f4450277f56290fc2600f13a0febb364c4
wackloner <wackloner@gmail.com>
2017-10-28 02:45:05 +0300

    solve Codeforces problem 159C + unit tests

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,81 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+data class Request(
+        val p: Int,
+        val c: Char
+)
+
+class Solver {
+    class SegmentTree(
+            private val size: Int
+    ) {
+        private val tree = Array(size * 4, { 0 })
+
+        private fun getPositionOf(v: Int, l: Int, r: Int, k: Int): Int? {
+            if (l == r)
+                return if (tree[v] == 0) null else l
+            val m = (l + r) / 2
+            return when (tree[v * 2 + 1] >= k) {
+                true -> getPositionOf(v * 2 + 1, l, m, k)
+                false -> getPositionOf(v * 2 + 2, m + 1, r, k - tree[v * 2 + 1])
+            }
+        }
+
+        fun getPositionOf(k: Int) = getPositionOf(0, 0, size - 1, k)
+
+        private fun add(v: Int, l: Int, r: Int, position: Int, delta: Int) {
+            if (l == r)
+                tree[v] += delta
+            else {
+                val m = (l + r) / 2
+                if (position <= m) {
+                    add(v * 2 + 1, l, m, position, delta)
+                } else {
+                    add(v * 2 + 2, m + 1, r, position, delta)
+                }
+                tree[v] = tree[v * 2 + 1] + tree[v * 2 + 2]
+            }
+        }
+
+        fun add(position: Int, delta: Int) = add(0, 0, size - 1, position, delta)
+    }
+
+    fun solve(k: Int, s: String, requests: Array<Request>): String {
+        val length = k * s.length
+        val trees = Array(26, { SegmentTree(length) })
+        for (i in 0 until k)
+            for (j in 0 until s.length) {
+                val index = s[j] - 'a'
+                val position = i * s.length + j
+                trees[index].add(position, 1)
+            }
+        for ((p, c) in requests) {
+            val index = c - 'a'
+            val position = trees[index].getPositionOf(p)
+            trees[index].add(position!!, -1)
+        }
+        val totalString = Array(length, { ' ' })
+        for (letter in 'a'..'z') {
+            val index = letter - 'a'
+            while (true) {
+                val position = trees[index].getPositionOf(1)
+                position ?: break
+                totalString[position] = letter
+                trees[index].add(position, -1)
+            }
+        }
+        return totalString.filter { c -> c != ' ' }.toCharArray().joinToString("")
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val input = Scanner(System.`in`)
+    val k = input.nextInt()
+    val s = input.next()
+    val n = input.nextInt()
+    val requests = Array(n,
+            { Request(input.nextInt(), input.next().single()) })
+    println(Solver().solve(k, s, requests))
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -4,7 +4,41 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun firstSample() {
+        val actual = Solver().solve(
+                2,
+                "bac",
+                arrayOf(
+                        Request(2, 'a'),
+                        Request(1, 'b'),
+                        Request(2, 'c')
+                )
+        )
+        assertEquals("acb", actual)
+    }
+
+    @Test
+    fun secondSample() {
+        val actual = Solver().solve(
+                1,
+                "abacaba",
+                arrayOf(
+                        Request(1, 'a'),
+                        Request(1, 'a'),
+                        Request(1, 'c'),
+                        Request(2, 'b')
+                )
+        )
+        assertEquals("baa", actual)
+    }
+
+    @Test
+    fun customTest() {
+        val actual = Solver().solve(
+                2000,
+                "aaaaaaaaaa",
+                Array(20000, { Request(1, 'a') })
+        )
+        assertEquals("", actual)
     }
 }
